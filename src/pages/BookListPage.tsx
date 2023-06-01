e368c3a0397318ad91aad89c732185b140718e86
Camino Losada <camino.losada.alonso@gmail.com>
2023-06-01 12:56:49 +0200

    Add dispatch to BookListPage

--- src/pages/BookListPage.tsx
+++ src/pages/BookListPage.tsx
@@ -1,6 +1,12 @@
+import { booksMocks } from "../mocks/booksMock";
+import { useAppDispatch } from "../store";
+import { loadBooksActionCreator } from "../store/books/booksSlice";
 import BookListPageStyled from "./BookListPageStyled";
 
 const BookListPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+
+  dispatch(loadBooksActionCreator(booksMocks));
   return (
     <BookListPageStyled>
       <h1 className="title">What is in your shelf?</h1>
