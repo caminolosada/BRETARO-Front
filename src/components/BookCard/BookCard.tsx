396b9051f93629865f0e15b9182f98786fe44907
Camino Losada <camino.losada.alonso@gmail.com>
2023-06-02 10:20:41 +0200

    Add general Button component

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -7,19 +7,21 @@ interface BookCardProps {
 
 const BookCard = ({ bookProps }: BookCardProps): React.ReactElement => {
   return (
-    <BookCardStyled>
-      <img
-        src={bookProps.frontPage}
-        alt={`${bookProps.title} front page`}
-        className="card-front-page"
-        width="90"
-        height="120"
-      />
-      <div className="info-book">
-        <h2 className="info-book__title">{bookProps.title}</h2>
-        <h3 className="info-book__author">{bookProps.author}</h3>
-      </div>
-    </BookCardStyled>
+    <>
+      <BookCardStyled>
+        <img
+          src={bookProps.frontPage}
+          alt={`${bookProps.title} front page`}
+          className="card-front-page"
+          width="90"
+          height="120"
+        />
+        <div className="info-book">
+          <h2 className="info-book__title">{bookProps.title}</h2>
+          <h3 className="info-book__author">{bookProps.author}</h3>
+        </div>
+      </BookCardStyled>
+    </>
   );
 };
 
