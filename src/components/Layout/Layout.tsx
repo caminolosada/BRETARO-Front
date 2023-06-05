badd6e2c65ebeffaab83776451a48702b649d20a
Camino Losada <camino.losada.alonso@gmail.com>
2023-06-05 13:16:01 +0200

    Move Loader rendering from BookListPage to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,10 +3,14 @@ import ContainerStyled from "../shared/ContainerStyled";
 import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
 import BookListPage from "../../pages/BookListPage";
+import { useAppSelector } from "../../store";
+import Loader from "../Loader/Loader";
 
 const Layout = (): React.ReactElement => {
+  const isLoading = useAppSelector((state) => state.ui.isLoading);
   return (
     <>
+      {isLoading && <Loader />}
       <ContainerStyled>
         <Header />
         <BookListPage />
