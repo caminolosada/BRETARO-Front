43e5820b7ede36e81ed1e307bafe2aa9171de6e4
Camino Losada <camino.losada.alonso@gmail.com>
2023-06-09 11:10:43 +0200

    Add Form and its styles

---
+++ src/components/Form/FormStyled.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+
+  .form {
+    &__container {
+      display: flex;
+      flex-direction: column;
+      background-color: ${(props) => props.theme.colors.primary};
+      border-radius: 10px;
+      padding: 20px;
+      gap: 20px;
+    }
+
+    &__label {
+      font-size: ${(props) => props.theme.fontSize.small};
+    }
+
+    &__input {
+      font-family: ${(props) => props.theme.fonts.primary};
+      border-radius: 5px;
+      background-color: ${(props) => props.theme.colors.background};
+      border: none;
+      height: 48px;
+      padding: 20px 16px;
+    }
+
+    &__select {
+      font-family: ${(props) => props.theme.fonts.primary};
+      border-radius: 5px;
+      background-color: ${(props) => props.theme.colors.background};
+      border: none;
+      height: 48px;
+      padding: 0px 16px;
+    }
+
+    &__option {
+      height: 48px;
+    }
+
+    &__button {
+      margin: 0 auto;
+      margin-bottom: 150px;
+      background-color: ${(props) => props.theme.colors.tertiary};
+      width: 162px;
+      height: 48px;
+      border-radius: 5px;
+      font-size: ${(props) => props.theme.fontSize.medium};
+      filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+`;
+
+export default FormStyled;
--- src/pages/AddBookPage/AddBookPageStyled.ts
+++ src/pages/AddBookPage/AddBookPageStyled.ts
@@ -3,11 +3,12 @@ import styled from "styled-components";
 const AddBookPageStyled = styled.main`
   display: flex;
   flex-direction: column;
+  gap: 40px;
 
   .title {
     color: ${(props) => props.theme.colors.generalText};
     font-size: ${(props) => props.theme.fontSize.medium};
-    margin-top: 70px;
+    margin-top: 30px;
   }
 `;
 
