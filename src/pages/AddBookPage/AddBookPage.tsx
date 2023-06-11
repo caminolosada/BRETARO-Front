f6c09b44486f34b0083c41a4ab29938139360ca0
Camino Losada <camino.losada.alonso@gmail.com>
2023-06-11 13:41:40 +0200

    Add postive feedback and navigation to home when create a book

--- src/pages/AddBookPage/AddBookPage.tsx
+++ src/pages/AddBookPage/AddBookPage.tsx
@@ -1,13 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import Form from "../../components/Form/Form";
+import modalData from "../../components/Modal/modalData";
 import useBooks from "../../hooks/useBooks/useBooks";
 import { useAppDispatch } from "../../store";
 import { addBooksActionCreator } from "../../store/books/booksSlice";
+import { showModalActionCreator } from "../../store/ui/uiSlice";
 import { BookStructure } from "../../types";
 import AddBookPageStyled from "./AddBookPageStyled";
+import paths from "../../routers/paths/paths";
 
 const AddBookPage = (): React.ReactElement => {
   const { createBook } = useBooks();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const onSubmit = async (bookData: BookStructure) => {
     const newBook = await createBook(bookData);
@@ -15,6 +20,16 @@ const AddBookPage = (): React.ReactElement => {
     if (newBook) {
       dispatch(addBooksActionCreator(newBook));
     }
+
+    navigate(paths.home);
+
+    dispatch(
+      showModalActionCreator({
+        isVisible: true,
+        isError: false,
+        message: modalData.message.okAdd,
+      })
+    );
   };
   return (
     <AddBookPageStyled>
