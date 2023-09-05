a149d2967dff092120600cf035fe67a195ef362e
Camino Losada <camino.losada.alonso@gmail.com>
2023-09-05 16:37:58 +0200

    Add Modify button and its styles on BookDetailPage

--- src/pages/BookDetailPage/BookDetailPage.tsx
+++ src/pages/BookDetailPage/BookDetailPage.tsx
@@ -1,15 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useBooks from "../../hooks/useBooks/useBooks";
 import { useAppDispatch, useAppSelector } from "../../store";
 import BookDetailPageStyled from "./BookDetailPageStyled";
 import { loadSelectedBookActionCreator } from "../../store/books/booksSlice";
 import { BookDataStructure } from "../../types";
 import { useEffect, useState } from "react";
+import Button from "../../components/Button/Button";
+import paths from "../../routers/paths/paths";
 
 const BookDetailPage = (): React.ReactElement => {
   const { id } = useParams();
   const { getMyBook } = useBooks();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const myBook = useAppSelector((state) => state.books.selectedBook);
   const [rating, setRating] = useState<string[]>([]);
 
@@ -28,6 +31,10 @@ const BookDetailPage = (): React.ReactElement => {
   useEffect(() => {
     window.scroll(0, 0);
   });
+
+  const handleOnModify = () => {
+    navigate(paths.editBook);
+  };
 
   return (
     <BookDetailPageStyled>
@@ -65,6 +72,14 @@ const BookDetailPage = (): React.ReactElement => {
           <p>{myBook.cosmos}</p>
         </div>
       </div>
+
+      <Button
+        classname="modify__button"
+        ariaLabel="modify"
+        title="modify"
+        text="Modify"
+        actionOnClick={handleOnModify}
+      />
     </BookDetailPageStyled>
   );
 };
