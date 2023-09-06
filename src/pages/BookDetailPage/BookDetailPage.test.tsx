be7e9e07475bd23eeabde84acc5ec361812ff5e5
Camino Losada <camino.losada.alonso@gmail.com>
2023-09-06 17:33:57 +0200

    Add use cases to BookDetailPage.test.tsx to test handleOnModify function

--- src/pages/BookDetailPage/BookDetailPage.test.tsx
+++ src/pages/BookDetailPage/BookDetailPage.test.tsx
@@ -1,7 +1,11 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { renderWithProviders, wrapWithRouter } from "../../utils/testUtils";
 import BookDetailPage from "./BookDetailPage";
-import { addedBookMock } from "../../mocks/booksMock";
+import { addedBookMock, booksMocks } from "../../mocks/booksMock";
+import paths from "../../routers/paths/paths";
+import UpdateBookPage from "../UpdateBookPage/UpdateBookPage";
 
 describe("Given a BookDetailPage", () => {
   describe("When it is rendered", () => {
@@ -29,6 +33,47 @@ describe("Given a BookDetailPage", () => {
       const text = screen.getByText(expectedText);
 
       expect(text).toBeInTheDocument;
+    });
+  });
+  describe("When an user clicks on the Modify button", () => {
+    test("Then it should navigate to the UpdateBookPage, whose title is 'What do you want to edit?'", async () => {
+      const expectedPageTitle = "What do you want to edit?";
+      const buttonText = "Modify";
+
+      const routes = [
+        {
+          path: paths.detail,
+          element: <BookDetailPage />,
+        },
+
+        {
+          path: "/",
+          element: <UpdateBookPage />,
+        },
+      ];
+
+      const router = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const modifyButton = screen.getByText(buttonText);
+      await userEvent.click(modifyButton);
+
+      const pageTitle = screen.getByRole("heading", {
+        name: expectedPageTitle,
+      });
+      expect(pageTitle).toBeInTheDocument();
+    });
+    test("Then it should navigate to the UpdateBookPage and this button should disapear", async () => {
+      const buttonText = "Modify";
+
+      renderWithProviders(wrapWithRouter(<BookDetailPage />), {
+        books: { booksData: booksMocks, selectedBook: booksMocks[0] },
+      });
+      const button = screen.getByText(buttonText);
+
+      await userEvent.click(button);
+      expect(button).not.toBeInTheDocument();
     });
   });
 });
